56185215c7a62dd6365bfbe9c2446e2b93d5769c
Павел Гомозов <gomozov.pavel@yandex.ru>
2021-01-31 20:07:36 +0300

    choose and play song

--- main.py
+++ main.py
@@ -3,28 +3,38 @@ import pyglet
 import sqlite3
 from random import randint
 
-eel.init("web")
-connection = sqlite3.connect('songs_db.db')
-
 
 def choose_random_song(connection):
     cursor = connection.cursor()
     cursor.execute('SELECT * FROM songs_db')
     amount_songs = len(cursor.fetchall())
     random_id = randint(1, amount_songs)
-    cursor.execute(f'SELECT path FROM songs_db WHERE id={random_id}')
-    songs = cursor.fetchall()
-    return songs[-1][-1]
+    cursor.execute(f'SELECT * FROM songs_db WHERE id={random_id}')
+    song = cursor.fetchall()
+    return song
 
 
-def play_song(path):
-    song_path = choose_random_song(connection)
+def play_song():
+    connection = sqlite3.connect('songs_db.db')
+    song = choose_random_song(connection)
+    name = song[0][1]
+    artist = song[0][2]
+    song_path = song[0][3]
     song = pyglet.media.load(song_path)
     song.play()
     pyglet.app.run()
 
 
+@eel.expose
+def start_game():
+    while True:
+        play_song()
+
+
 def main():
     eel.init("web")
-    connection = sqlite3.connect('songs_db.db')
     eel.start('home.html', size=(700, 400))
+
+
+if __name__ == '__main__':
+    main()
