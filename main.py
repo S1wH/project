1806e483b35599338d6afd13607f4c441ca811aa
Павел Гомозов <gomozov.pavel@yandex.ru>
2021-02-14 14:10:39 +0300

    1

--- main.py
+++ main.py
@@ -3,15 +3,20 @@ import sqlite3
 from random import randint
 import playsound
 sp = []
+played_songs = []
 
 
 def choose_random_song(connection):
     cursor = connection.cursor()
     cursor.execute('SELECT * FROM songs_db')
     amount_songs = len(cursor.fetchall())
-    random_id = randint(1, amount_songs)
-    cursor.execute(f'SELECT * FROM songs_db WHERE id={random_id}')
-    song = cursor.fetchall()
+    while True:
+        random_id = randint(1, amount_songs)
+        cursor.execute(f'SELECT * FROM songs_db WHERE id={random_id}')
+        song = cursor.fetchall()
+        if song[0][1] not in played_songs:
+            break
+    played_songs.append(song[0][1])
     return song
 
 
@@ -24,11 +29,11 @@ def play_song():
     song_path = song[0][3]
     playsound.playsound(song_path, False)
     sp.append(name + ' ' + artist)
-    print(sp[0])
 
 
 @eel.expose
 def check_song(string):
+    print(string)
     if string == sp[0]:
         return True
     return False
